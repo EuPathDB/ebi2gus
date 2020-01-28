de684f5b1b7297b5a1d7ffbd4e3e59d808cfc7a3
John Brestelli <jbrestell@gmail.com>
2020-01-28 16:20:09 -0500

    one gene has an isoelectric point of None

--- lib/perl/GUS/ApiDB/AaSequenceAttribute.pm
+++ lib/perl/GUS/ApiDB/AaSequenceAttribute.pm
@@ -7,11 +7,18 @@ sub init {
     my ($self, $translation, $gusTranslatedAASequence) = @_;
 
     my $molWeight = $translation->get_all_Attributes("MolecularWeight")->[0]->value();
+
+
+    my $isoelectricPoint = $translation->get_all_Attributes("IsoPoint")->[0]->value();
+
+    if(lc $isoelectricPoint eq 'none') {
+	$isoelectricPoint = undef;
+    }
     
     return {aa_sequence_id => $gusTranslatedAASequence->getPrimaryKey(),
 	    min_molecular_weight => $molWeight,
 	    max_molecular_weight => $molWeight,
-	    isoelectric_point => $translation->get_all_Attributes("IsoPoint")->[0]->value(),	    
+	    isoelectric_point => $isoelectricPoint,
     };
 }
 
