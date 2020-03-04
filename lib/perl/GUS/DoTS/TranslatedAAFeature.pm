9d6c291d7b9d06c1f326427518270d5e4e84d82a
John Brestelli <jbrestell@gmail.com>
2020-03-04 15:34:38 -0500

    fix translation start and stop

--- lib/perl/GUS/DoTS/TranslatedAAFeature.pm
+++ lib/perl/GUS/DoTS/TranslatedAAFeature.pm
@@ -7,17 +7,13 @@ sub init {
     my ($self, $transcript, $gusTranslatedAASequence, $gusTranscript, $gusExternalDatabaseRelease) = @_;
 
     my $gusTranslatedAASequenceAsHash  = $gusTranslatedAASequence->getGUSRowAsHash();
-    
-    my $translation = $transcript->translation();
-
-    my $translationStop = $translation->cdna_end() - $translation->cdna_start() + 1;
 
     return {aa_sequence_id => $gusTranslatedAASequence->getPrimaryKey(),
 	    na_feature_id => $gusTranscript->getPrimaryKey(),
 	    subclass_view => "TranslatedAAFeature",
 	    source_id => $gusTranslatedAASequenceAsHash->{source_id},
-	    translation_start => 1,
-	    translation_stop => $translationStop,
+	    translation_start => $transcript->cdna_coding_start(),
+	    translation_stop => $transcript->cdna_coding_end(),
 	    is_predicted => 0,
 	    external_database_release_id => $gusExternalDatabaseRelease->getPrimaryKey(),
     };
