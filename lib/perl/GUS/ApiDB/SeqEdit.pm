b4211426c14c075abdfcf1521d60409192da0a60
John Brestelli <jbrestell@gmail.com>
2020-07-29 15:58:10 -0400

    add original sequence to seqedit table

--- lib/perl/GUS/ApiDB/SeqEdit.pm
+++ lib/perl/GUS/ApiDB/SeqEdit.pm
@@ -23,16 +23,45 @@ sub init {
 	die "only transcript or translation seqedit types are supported";
     }
 
+    my $originalSequence = $self->getInitialSequence($seqEdit, $transcript, $sequenceType);
+
     return {source_id => $sourceId,
 	    sequence_type => $sequenceType,
 	    sequence_ontology_id => $sequenceOntologyId,
-	    trans_start => $seqEdit->start(),
-	    trans_end => $seqEdit->end(),
+	    trans_start => $seqEdit->start(), 
+	    trans_end => $seqEdit->end(), 
 	    sequence => $seqEdit->alt_seq(),
+	    orig_sequence => $originalSequence, 
 	    start_min => $coord->start(),
 	    end_max => $coord->end(),
 	    length_diff => $seqEdit->length_diff(),
     };
+}
+
+sub getInitialSequence {
+    my ($self, $seqEdit, $transcript, $sequenceType) = @_;
+
+    # turn off edits
+    $transcript->edits_enabled(0);
+
+    my $sequence;
+    if($sequenceType eq 'translation') {
+	$sequence = $transcript->translate()->seq();
+    }
+    elsif($sequenceType eq 'translation') {
+	$sequence = $transcript->spliced_seq();
+    }
+    else {
+	die "only transcript or translation seqedit types are supported";
+    }
+
+    my $len = $seqEdit->end() - $seqEdit->start() + 1;
+    my $substr = substr($sequence, $seqEdit->start - 1, $len);
+
+    # turn back on edits!
+    $transcript->edits_enabled(1);
+    
+    return $substr;
 }
 
 1;
