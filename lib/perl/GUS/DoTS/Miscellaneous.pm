dd9d1b413bd89730482c07fe5173586ebcd9102d
Sufen Hu <sufenMac@l10893.home>
2021-01-15 18:19:12 -0500

    pass a seqOntologyName argument to DoTS::Miscellaneous

--- lib/perl/GUS/DoTS/Miscellaneous.pm
+++ lib/perl/GUS/DoTS/Miscellaneous.pm
@@ -5,13 +5,17 @@ use strict;
 
 
 sub init {
-    my ($self, $name, $gusExternalNASequence, $gusExternalDatabaseRelease, $sequenceOntologyId) = @_;
+    my ($self, $name, $gusExternalNASequence, $gusExternalDatabaseRelease, $sequenceOntologyId, $seqOntologyName) = @_;
 
     print "IN INIT\n";
     
     return {na_sequence_id => $gusExternalNASequence->getPrimaryKey(),
 	    subclass_view => 'Miscellaneous',
+<<<<<<< HEAD
 	    name => $name,
+=======
+	    name => $seqOntologyName,
+>>>>>>> 1fdfc71... pass a seqOntologyName argument to DoTS::Miscellaneous
 	    source_id => $name,
 	    sequence_ontology_id => $sequenceOntologyId,
 	    external_database_release_id => $gusExternalDatabaseRelease->getPrimaryKey(),
