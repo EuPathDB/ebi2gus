ac856f9d28491597955b7efabee62fe348012add
Mustafa Nural <mvnural@gmail.com>
2023-08-23 13:27:25 -0400

    EnzymeClass use KEGG_Enzyme database for the external_database_release_id.
    (cherry picked from commit decd433732fb5d926c948b270187ceccef13d950)

--- lib/perl/EBIParser.pm
+++ lib/perl/EBIParser.pm
@@ -743,7 +743,7 @@ sub parseTranslation {
 	GUS::DoTS::DbRefAAFeature->new($gusTableWriters, $dbRefId, $gusTranslatedAAFeature->getPrimaryKey());
 
 	if($databaseName eq 'KEGG_Enzyme') {
-	    $self->parseKeggEnzyme($primaryId, $gusTranslatedAASequence->getPrimaryKey(), $databaseName);
+	    $self->parseKeggEnzyme($primaryId, $gusTranslatedAASequence->getPrimaryKey(), $databaseName, $externalDatabaseReleaseId);
 	}
 	
     }
@@ -759,7 +759,7 @@ sub parseTranslation {
 
 
 sub parseKeggEnzyme {
-    my ($self, $keggEnzyme, $gusAASequenceId, $databaseName) = @_;
+    my ($self, $keggEnzyme, $gusAASequenceId, $databaseName, $externalDatabaseReleaseId) = @_;
 
     my $gusTableWriters = $self->getGUSTableWriters();
     
@@ -769,7 +769,7 @@ sub parseKeggEnzyme {
     foreach my $ec (@ecNumbers) {
 	my $gusEnzymeClassId = $seenEnzymeClasses{$ec};
 	unless($gusEnzymeClassId) {
-	    $gusEnzymeClassId = GUS::SRes::EnzymeClass->new($gusTableWriters, $ec)->getPrimaryKey();
+	    $gusEnzymeClassId = GUS::SRes::EnzymeClass->new($gusTableWriters, $ec, $externalDatabaseReleaseId)->getPrimaryKey();
 	}
 
 	GUS::DoTS::AASequenceEnzymeClass->new($gusTableWriters, $gusAASequenceId, $gusEnzymeClassId, $databaseName)->getPrimaryKey();
